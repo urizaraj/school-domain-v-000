3528d8e53ca25470990b3268395953fcf99fdb06
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 16:03:20 +0000

    automatically backed up by learn

---
+++ lib/school.rb
@@ -0,0 +1,16 @@
+class School
+  attr_reader :name
+
+  def initalized(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  
